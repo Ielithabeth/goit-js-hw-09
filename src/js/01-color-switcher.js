028115e8a33dd3a835fe54a48347c3443a9a6e11
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-21 23:10:56 +0200

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled', '');
+};
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', '');
+    stopBtn.setAttribute('disabled', '');
+}
