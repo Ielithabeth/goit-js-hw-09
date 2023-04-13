51bb9bbed2fb1d9ce811637cb07a9bd1fbe04b7f
Ielithabeth <lisa.pltrtskaya@gmail.com>
2023-04-13 22:05:14 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let interval = null;
+
+// startBtn.style.
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const onStartClick = () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    body.style.backgroundColor = getRandomHexColor();
+
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+const onStopClick = () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(interval);
+}
+
+startBtn.addEventListener("click", onStartClick);
+stopBtn.addEventListener("click", onStopClick);
 
